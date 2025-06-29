f5fe1277bf011280376b00785b13a3a4caad2bb6
varungarg2796 <varungarg2796@hotmail.com>
2025-06-30 00:30:30 +0530

    changing import again

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
